b8011090b79160c49ad38679b83a3aeda4a68a95
AlexiaGu <alexiaguidet@gmail.com>
2023-12-12 10:01:35 +0100

    ajout components.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,13 +2,34 @@
 // import pokemonCard from "./components/pokemonCard"
 
 function pokemonCard() {
+
+    const pokemon = {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+        return <p> {pokemon === this.imgSrc ? "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" : "???"} </p>
+    }
+
     return (
       <figure>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
         <figcaption>Bulbasaur</figcaption>
       </figure>
       
+
     );
   }
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   
   export default pokemonCard;
