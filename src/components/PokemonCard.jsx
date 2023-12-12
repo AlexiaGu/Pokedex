8892cee6df06ced972cf773bc3c65649b89e717d
AlexiaGu <alexiaguidet@gmail.com>
2023-12-12 16:26:08 +0100

    compo dynamique en fonction de l'index du tableau

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,27 @@
 // import "./App.css";
 // import pokemonCard from "./components/pokemonCard"
 
-function pokemonCard() {
-    return (
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
-      
-    );
-  }
+function PokemonCard() {
+
+const pokemon = pokemonList[1]
   
-  export default pokemonCard;
+return (
+  <figure>
+    {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc}></img> : <p>???</p>}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
+)
+} 
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  export default PokemonCard;
